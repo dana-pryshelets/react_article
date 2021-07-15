b02c3c042b679c0e39021ba8652a3fe0bc072625
Dana Pryshelets <tanyarud53@gmail.com>
2021-07-15 20:13:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.3e63aab5c9aff0a766aafa4757855f3a.js"
 );
 
 self.addEventListener('message', (event) => {
